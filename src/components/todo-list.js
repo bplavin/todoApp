604d3462f7e14dba42c8ff62f364849dc59c4b26
Bogdan Plavin <bohdan.plavin@gmail.com>
2021-08-18 22:17:06 -0500

    Added collection and keys to the properties

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -2,13 +2,24 @@ import React from 'react';
 
 import TodoListItem from './todo-list-item';
 
-const TodoList = () => {
+const TodoList = ({todos}) => {
+  
+  const elements = todos.map((item) => {
+
+    const {id, ...itemProps} = item;
+
+    return (
+      <li key={id}>
+        {/* Destructuring each element of the array 'todos'
+        by using spred operator*/}
+        <TodoListItem {... itemProps} /> 
+      </li>
+    );
+  });
+
   return (
     <ul>
-      <li><TodoListItem label='Drink Coffee' /></li>
-      <li><TodoListItem 
-      label='Create React App'
-      important/></li>
+      {elements}
     </ul>
   );
 };
